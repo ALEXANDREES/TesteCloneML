3a435083038654f091e422323871e5071548bbaa
ALEXANDREES <alexandrehenrique.es@gmail.com>
2021-03-28 15:33:18 -0400

    Upando Projeto

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,114 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          color="black"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+
+        <q-toolbar-title>
+          <q-input rounded outlined dense color="grey" bg-color="white" label="Buscar no Mercado Livre" class="q-ma-md">
+            <template v-slot:prepend>
+              <q-icon name="search"/>
+            </template>
+          </q-input>
+        </q-toolbar-title>
+
+        <div> <q-btn round flat icon="shopping_cart" text-color="black" size="15px"/> </div>
+      </q-toolbar>
+
+        <div class="row no-wrap items-center"> <q-btn round flat align="right" class="q-ml-sm q-mb-sm" icon="location_on" text-color="black" size="8px"/> <p class="q-ml-sm q-mb-sm" style="color:black"> Enviar para Alexandre Henrique Baratelli de Oliveira </p> <q-btn round flat align="left" class="q-ml-sm q-mb-sm" icon="navigate_next" text-color="black" size="10px"/> </div>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-1"
+    >
+      <q-list>
+        <q-item-label
+          header
+          class="text-grey-8"
+        >
+          Essential Links
+        </q-item-label>
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from 'components/EssentialLink.vue'
+
+const linksData = [
+  {
+    title: 'Docs',
+    caption: 'quasar.dev',
+    icon: 'school',
+    link: 'https://quasar.dev'
+  },
+  {
+    title: 'Github',
+    caption: 'github.com/quasarframework',
+    icon: 'code',
+    link: 'https://github.com/quasarframework'
+  },
+  {
+    title: 'Discord Chat Channel',
+    caption: 'chat.quasar.dev',
+    icon: 'chat',
+    link: 'https://chat.quasar.dev'
+  },
+  {
+    title: 'Forum',
+    caption: 'forum.quasar.dev',
+    icon: 'record_voice_over',
+    link: 'https://forum.quasar.dev'
+  },
+  {
+    title: 'Twitter',
+    caption: '@quasarframework',
+    icon: 'rss_feed',
+    link: 'https://twitter.quasar.dev'
+  },
+  {
+    title: 'Facebook',
+    caption: '@QuasarFramework',
+    icon: 'public',
+    link: 'https://facebook.quasar.dev'
+  },
+  {
+    title: 'Quasar Awesome',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  }
+]
+
+export default {
+  name: 'MainLayout',
+  components: { EssentialLink },
+  data () {
+    return {
+      leftDrawerOpen: false,
+      essentialLinks: linksData
+    }
+  }
+}
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,90 @@
+<template>
+  <q-page style="background-color: #E5E4E2">
+    <div class="" style="background-color:yellow">
+      <div class="q-mx-sm">
+        <q-carousel
+          animated
+          swipeable
+          v-model="slide"
+          arrows
+          navigation
+          infinite
+          height="200px"
+        >
+          <q-carousel-slide :name="1" img-src="https://cdn.quasar.dev/img/mountains.jpg" />
+          <q-carousel-slide :name="2" img-src="https://cdn.quasar.dev/img/parallax1.jpg" />
+          <q-carousel-slide :name="3" img-src="https://cdn.quasar.dev/img/parallax2.jpg" />
+          <q-carousel-slide :name="4" img-src="https://cdn.quasar.dev/img/quasar.jpg" />
+        </q-carousel>
+      </div>
+    </div>
+
+    <div class="row justify-between q-mx-md q-my-md">
+      <div class="col justify-between q-mx-md q-my-md">
+        <q-btn round color="white" icon="qr_code_2" text-color="black" size="20px"/>
+        <p style="font-size: 12px"> Pagar com código QR </p>
+      </div>
+
+      <div class="col justify-between q-mx-md q-my-md">
+        <q-btn round color="white" icon="sell" text-color="black" size="20px"/>
+        <p style="font-size: 12px"> Ofertas do dia </p>
+      </div>
+
+      <div class="col justify-between q-mx-md q-my-md">
+        <q-btn round color="white" icon="shopping_basket" text-color="black" size="20px"/>
+        <p style="font-size: 12px"> Supermercado </p>
+      </div>
+
+      <div class="col justify-between q-mx-md q-my-md">
+        <q-btn round color="white" icon="drive_eta" text-color="black" size="20px"/>
+        <p style="font-size: 12px"> Carros, motos e outros </p>
+      </div>
+
+      <div class="col justify-between q-mx-md q-my-md">
+        <q-btn round color="white" icon="add" text-color="black" size="20px"/>
+        <p style="font-size: 12px"> Ver mais </p>
+      </div>
+    </div>
+
+    <div>
+      <q-card class="q-mx-sm">
+        <q-card-section>
+          <div>
+            <p class="text-h6"> Visto recentemente </p>
+            <q-separator grey/>
+          </div>
+          <div>
+            <q-img src="https://cdn.quasar.dev/img/parallax2.jpg" basic/>
+          </div>
+        </q-card-section>
+
+        <div class="row q-mx-md">
+          <p>Fusível Cerâmico 4a X 250v /5x20mm/com Retardo - Kit 3pçs</p>
+        </div>
+
+        <div class="row q-mx-md">
+          <p class="text-h6" style="bold">R$ 23,75</p>
+        </div>
+
+        <q-separator dark />
+
+        <div>
+          <q-card-actions>
+            <q-btn flat align="between" class="full-width" text-color="blue" icon-right="arrow_right" label="Ver histórico de navegação"/>
+          </q-card-actions>
+        </div>
+      </q-card>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+      slide: 1
+    }
+  }
+}
+</script>
